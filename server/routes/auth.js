5a47ff41eab8f68ba10bb8d29ef4a2f2feaf98a9
oriemickelsen <omickelsen@gmail.com>
2025-03-06 12:16:59 -0700

    rollback auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,7 +6,6 @@ const { OAuth2Client } = require('google-auth-library');
 
 dotenv.config({ path: require('path').resolve(__dirname, '../../.env') });
 
-
 const oauth2Client = new OAuth2Client(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET
@@ -17,17 +16,13 @@ const ADMIN_EMAILS = ['omickelsen@gmail.com', 'mickelsenfamilyfarms@gmail.com'];
 // Redirect to Google for authentication
 router.get('/google', (req, res) => {
   const redirectUri = process.env.NODE_ENV === 'production'
-    ? [
-        'https://mickelsen-family-farms.herokuapp.com/auth/google/callback',
-        'https://mickelsenfamilyfarms.com/auth/google/callback',
-        'https://www.mickelsenfamilyfarms.com/auth/google/callback',
-      ][Math.floor(Math.random() * 3)] // Randomly select one for load balancing
+    ? 'https://mickelsen-family-farms.herokuapp.com/auth/google/callback'
     : 'http://localhost:5000/auth/google/callback';
   const redirectUrl = oauth2Client.generateAuthUrl({
     scope: ['profile', 'email', 'https://www.googleapis.com/auth/calendar'],
     redirect_uri: redirectUri,
   });
-  
+  console.log('Redirecting to Google with URL:', redirectUrl);
   res.redirect(redirectUrl);
 });
 
@@ -38,11 +33,7 @@ router.get('/google/callback', async (req, res) => {
     const { tokens } = await oauth2Client.getToken({
       code,
       redirect_uri: process.env.NODE_ENV === 'production'
-        ? [
-            'https://mickelsen-family-farms.herokuapp.com/auth/google/callback',
-            'https://mickelsenfamilyfarms.com/auth/google/callback',
-            'https://www.mickelsenfamilyfarms.com/auth/google/callback',
-          ][Math.floor(Math.random() * 3)] // Randomly select one
+        ? 'https://mickelsen-family-farms.herokuapp.com/auth/google/callback'
         : 'http://localhost:5000/auth/google/callback',
     });
     oauth2Client.setCredentials(tokens);
@@ -59,14 +50,10 @@ router.get('/google/callback', async (req, res) => {
       throw new Error('JWT_SECRET is not set in environment variables');
     }
     const token = jwt.sign({ email: userEmail, isAdmin }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    
+    console.log('Generated JWT:', token);
 
     const baseUrl = process.env.NODE_ENV === 'production'
-      ? [
-          'https://mickelsen-family-farms.herokuapp.com',
-          'https://mickelsenfamilyfarms.com',
-          'https://www.mickelsenfamilyfarms.com',
-        ][Math.floor(Math.random() * 3)] // Randomly select one
+      ? 'https://mickelsen-family-farms.herokuapp.com'
       : 'http://localhost:3000';
     res.redirect(`${baseUrl}/auth/success?token=${encodeURIComponent(token)}`);
   } catch (err) {
