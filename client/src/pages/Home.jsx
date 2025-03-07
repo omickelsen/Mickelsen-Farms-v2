ad02baa1d2f0cb45535e477ec3a7b9d8652a4d21
oriemickelsen <omickelsen@gmail.com>
2025-03-06 19:10:15 -0700

    added contact info for nik

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -61,7 +61,7 @@ function Home() {
           <div className="max-w-md mx-auto text-center text-gray-700">
             <p>1339 North 500 East, Pleasant Grove, UT 84062</p>
             <p>Email: mickelsenfamilyfarms@gmail.com</p>
-            <p>JoDee: +1 801-372-2070 | AL: +1 801-360-6071</p>
+            <p>JoDee: +1 801-372-2070 | AL: +1 801-360-6071 | Nik: +1 801-857-8636</p>
           </div>
         </div>
       </section>
